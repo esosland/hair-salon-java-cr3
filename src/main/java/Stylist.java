3e0d02e8a22ae3dfca377fb12e576e3131801420
Elissa Sosland <esosland@gmail.com>
2016-05-08 12:36:44 -0700

    add passing test for instanting stylist name correctly, add method to get stylist name

--- src/main/java/Stylist.java
+++ src/main/java/Stylist.java
@@ -9,4 +9,9 @@ public class Stylist {
     this.name = name;
   }
 
+  public String getName() {
+    return name;
+  }
+
+
 }
--- src/test/java/StylistTest.java
+++ src/test/java/StylistTest.java
@@ -25,4 +25,10 @@ public class StylistTest {
     assertEquals(true, myStylist instanceof Stylist);
   }
 
+  @Test
+  public void getName_stylistInstantiatesWithName_String() {
+    Stylist myStylist = new Stylist("Gonzo");
+    assertEquals("Gonzo", myStylist.getName());
+  }
+
 }
