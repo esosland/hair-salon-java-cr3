209bca1eed5fded9245125e0180811d33175bad5
Elissa Sosland <esosland@gmail.com>
2016-05-08 12:27:35 -0700

    add passing test for instantiating stylist object correctly

---
+++ src/main/java/Stylist.java
@@ -0,0 +1,12 @@
+import java.util.List;
+import org.sql2o.*;
+
+public class Stylist {
+  private int id;
+  private String name;
+
+  public Stylist(String name) {
+    this.name = name;
+  }
+
+}
---
+++ src/test/java/StylistTest.java
@@ -0,0 +1,28 @@
+import org.sql2o.*;
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class StylistTest {
+
+  @Before
+  public void setUp() {
+    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/hair_salon_test", null, null);
+  }
+
+  @After
+  public void tearDown() {
+    try(Connection con = DB.sql2o.open()) {
+      String deleteClientsQuery = "DELETE FROM clients *;";
+      String deleteStylistsQuery = "DELETE FROM stylists *;";
+      con.createQuery(deleteClientsQuery).executeUpdate();
+      con.createQuery(deleteStylistsQuery).executeUpdate();
+    }
+  }
+
+  @Test
+  public void Stylist_instantiatesCorrectly_true() {
+    Stylist myStylist = new Stylist("Kermit");
+    assertEquals(true, myStylist instanceof Stylist);
+  }
+
+}
