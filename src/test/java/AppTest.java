c703ad47d95f1976a9202dbba8291942f90c8fa9
Elissa Sosland <esosland@gmail.com>
2016-05-08 19:16:37 -0700

    add README with database instructions, add DatabaseRule to DRY up code

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -19,20 +19,8 @@ public class AppTest extends FluentTest {
   @ClassRule
   public static ServerRule server = new ServerRule();
 
-  @Before
-    public void setUp() {
-      DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/hair_salon_test", null, null);
-    }
-
-    @After
-    public void tearDown() {
-      try(Connection con = DB.sql2o.open()) {
-        String deleteClientsQuery = "DELETE FROM clients *;";
-        String deleteStylistsQuery = "DELETE FROM stylists *;";
-        con.createQuery(deleteClientsQuery).executeUpdate();
-        con.createQuery(deleteStylistsQuery).executeUpdate();
-      }
-    }
+  @Rule
+  public DatabaseRule database = new DatabaseRule();
 
     @Test
     public void rootTest() {
